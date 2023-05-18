4aa21600bb66ad27af21370fa90544e43128ef75
mannotrobot <boxforgit@gmail.com>
2023-05-18 13:09:03 +0300

    Рефакторизовано представления домашней страницы для использования шаблона

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -8,18 +9,14 @@ from lists.views import home_page
 
 class HomePageTest(TestCase):
     """ test home page """
-    def test_root_url_resolves_to_home_page_view(self):
-        """ тест: корневой url преобразуется во вьюшку дом
-            страницы """
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
+ 
 
     def test_home_page_returns_correct_html(self):
         """ тест: домашняя страница возращает правильный html"""
-        request = HttpRequest()
-        response = home_page(request)
+        response = self.client.get('/')
         html = response.content.decode('utf8')
         self.assertTrue(html.startswith('<html>'))
         self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+        self.assertTrue(html.strip().endswith('</html>'))
+        
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 def home_page(request):
     """home page my app"""
-    return HttpResponse('<html><title><h1>To-Do lists</h1></title></html>')
+    return render(request, 'home.html')
