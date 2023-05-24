48337cd7c00a20779f162b6164731557142ef4ad
mannotrobot <boxforgit@gmail.com>
2023-05-24 13:24:35 +0300

    Первые шаги ФТ для макетирования + стилевого оформления

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -121,6 +121,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Купить павлиньи перья', page_text)
         self.assertIn('Купить молоко', page_text)
 
+
+    def test_layout_and_styling(self):
+        """тест: макет и стиль оформления"""
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512,
+            delta=10,
+        )
+
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10,
+        )
 #        self.fail("ЗАКОНЧИТЬ ТЕСТ")
 
 
