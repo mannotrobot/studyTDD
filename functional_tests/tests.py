e8dbfc84e6f3b2b3b44acc51ccba70de9c492311
mannotrobot <boxforgit@gmail.com>
2023-05-24 21:59:04 +0300

    Настроен исполнитель ФТ для тестирования промежуточного сервера

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,3 +1,4 @@
+import os
 import time
 import unittest
 
@@ -18,8 +19,9 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         """установка"""
         self.browser = webdriver.Firefox()
-
-
+        staging_server = os.environ.get("STAGING_SERVER")
+        if staging_server:
+            self.live_server_url = 'http://' + staging_server
 
     def tearDown(self):
         """демонтаж"""
