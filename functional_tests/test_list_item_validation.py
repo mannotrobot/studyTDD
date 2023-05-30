a4161d29c2f2548503fc3f616b2c611b5de71e86
mannotrobot <boxforgit@gmail.com>
2023-05-30 23:04:36 +0300

    Добавлен скелет теста

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -9,4 +9,22 @@ class ItemValidationTest(FunctionalTest):
     @skip
     def test_cannot_add_empty_list_items(self):
         """тест: нельзя добавлять пустые элементы списка"""
-        pass
+        self.browser.get(self.live_server_url)
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys(Keys.Enter)
+        
+        
+        # домашная страница обновляется, появляется сообщение об ошибке,
+        # которое говорит, что элементы спсика не должны быть пустыми
+        self.assertEqual(
+            self.browser.find_element('.has_error').text,
+            "You cant't have an empty list item"
+        )   
+        
+        # новая попытка с неким текстом для элемента, и теперь все ок.
+        self.fail("Закончить текст!")
+
+        # Домашняя страница обновляется, и появляется сообщение об ошибке,
+        # которое говорит, что элементы списка не должны быть пустыми
+        self.wait_for(lambda: self.assertEqual( 
+        self.browser.find_element_by_css_selector('.has-error').text,
+        "You can't have an empty list item"))
