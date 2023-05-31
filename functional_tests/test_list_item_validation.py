aca9257f33a5dbc0ffb732bae3b7509832058736
mannotrobot <boxforgit@gmail.com>
2023-05-31 22:28:22 +0300

    Добавлен функ.тест на пустой список и с корректирован base.py

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,4 +1,5 @@
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from unittest import skip
 from .base import FunctionalTest
 
@@ -6,25 +7,37 @@ from .base import FunctionalTest
 
 class ItemValidationTest(FunctionalTest):
     """тест валидации элемента списка"""
-    @skip
+#    @skip
     def test_cannot_add_empty_list_items(self):
         """тест: нельзя добавлять пустые элементы списка"""
         self.browser.get(self.live_server_url)
-        self.browser.find_element(by=By.ID, value='id_new_item').send_keys(Keys.Enter)
-        
-        
-        # домашная страница обновляется, появляется сообщение об ошибке,
-        # которое говорит, что элементы спсика не должны быть пустыми
-        self.assertEqual(
-            self.browser.find_element('.has_error').text,
-            "You cant't have an empty list item"
-        )   
-        
-        # новая попытка с неким текстом для элемента, и теперь все ок.
-        self.fail("Закончить текст!")
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys(Keys.ENTER)
+
 
         # Домашняя страница обновляется, и появляется сообщение об ошибке,
         # которое говорит, что элементы списка не должны быть пустыми
-        self.wait_for(lambda: self.assertEqual( 
-        self.browser.find_element_by_css_selector('.has-error').text,
+        self.wait_for(lambda: self.assertEqual(
+        self.browser.find_element(By.CSS_SELECTOR, ".has-error").text,
         "You can't have an empty list item"))
+
+
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys('Buy milk')
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+
+        # повторная попытка отправить пустой элемент списка
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys(Keys.ENTER)
+
+
+        # повторное предупреждение на странице списка
+        self.wait_for(lambda: self.assertEqual(
+        self.browser.find_elemnt(By.CSS_SELECTOR, '.has-error').text,
+        "You can't have an empty list item"))
+
+
+        # отправка корректных данных
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys('Make tea')
+        self.browser.find_element(by=By.ID, value='id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Make tea')
