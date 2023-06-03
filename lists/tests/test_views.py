b3df4860dadf3c08fd81b7b96e8b96402b1befb0
mannotrobot <boxforgit@gmail.com>
2023-06-03 21:55:54 +0300

    Скорректировано новое представление списка с учетом валидации модели

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,6 +2,7 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+from django.utils.html import escape
 
 from lists.views import home_page
 from lists.models import Item, List
@@ -73,6 +74,24 @@ class NewListTest(TestCase):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         new_list = List.objects.first()
         self.assertRedirects(response, f'/lists/{new_list.id}/')
+
+
+    def test_validation_errors_are_sent_back_to_home_page_template(self):
+        """тест: оишбки валидации отсылают назад в шаблон дом. страницы"""
+        response = self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
+        expected_error = escape("You can't have an empty list item")
+        #print(response.content.decode())
+        self.assertContains(response, expected_error)
+
+
+    def test_invalid_list_items_arent_saved(self):
+        """тест: сохраняются недопустимые элементы списка"""
+        self.client.post('/lists/new', data={'item_text': ''})
+        self.assertEqual(List.objects.count(), 0)
+        self.assertEqual(Item.objects.count(), 0)
+
 
 
 
