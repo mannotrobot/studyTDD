6fa268ff29b0ee9c1740dba56eab6bee78e959c2
mannotrobot <boxforgit@gmail.com>
2023-06-30 00:54:10 +0300

    Использование собственного для формы метода save

--- lists/views.py
+++ lists/views.py
@@ -18,10 +18,11 @@ def view_list(request, list_id):
 
     if request.method == "POST":
         form = ItemForm(data=request.POST)
+
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
-  
+
     return render(request, 'list.html', {'list': list_, 'form': form})
 
 
@@ -30,18 +31,8 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        item = Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
-
-#    try:
-#        item.full_clean()
-#        item.save()
-#    except ValidationError:
-#        list_.delete()
-#        error = "You can't have an empty list item"
-#        return render(request, 'home.html', {"error": error})
-#
-#    return redirect(list_)
 
